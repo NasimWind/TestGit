34100a464bc4f31bef5cb2b87a2d9a1bef02a237
RS/PC/1508 <administrator@rspc1508s-imac.rssoftware.com>
2015-12-23 11:19:35 +0530

    Initial Commit

---
+++ TestGitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGitApp
+//
+//  Created by   RS/PC/1508 on 23/12/15.
+//  Copyright © 2015 RS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
