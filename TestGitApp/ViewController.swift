030576e05b478c787df8b6ac8e981537fc74aea6
RS/PC/1508 <administrator@rspc1508s-imac.rssoftware.com>
2015-12-23 12:51:02 +0530

    test

--- TestGitApp/ViewController.swift
+++ TestGitApp/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //test github
+        //commit
     }
 
     override func didReceiveMemoryWarning() {
